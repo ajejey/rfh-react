1f74e9c9e2e98878b42b223f99139b74973c31dd
Ajey <ajejey@gmail.com>
2023-02-06 22:42:11 +0530

    make payment redirect card

--- src/Components/PaymentRedirect.js
+++ src/Components/PaymentRedirect.js
@@ -5,6 +5,7 @@ function PaymentRedirect() {
     const navigate = useNavigate()
     const [paymentStatus, setPaymentStatus] = useState("")
     const [paymentDetails, setPaymentDetails] = useState({})
+    const [loading, setLoading] = useState(true)
 
     const handleOkayClick = () => {
         navigate('/')
@@ -12,6 +13,7 @@ function PaymentRedirect() {
 
     useEffect(() => {
         // if (paymentStatus === 'Payment Initiated') {
+        setLoading(true)
         const getPaymentStatus = async () => {
             try {
                 const response = await fetch("https://rfh-backend.up.railway.app/app/payment-status", {
@@ -24,6 +26,7 @@ function PaymentRedirect() {
                 console.log("status data", data);
                 setPaymentStatus(data?.message)
                 setPaymentDetails(data?.data)
+                setLoading(false)
             } catch (error) {
                 console.log(error)
             }
@@ -36,24 +39,54 @@ function PaymentRedirect() {
             <div style={{ height: "100vh", display: "flex", justifyContent: "center", alignItems: 'center', }}>
                 <div style={{ padding: "3%", boxShadow: "4px 8px 29px -10px rgba(0,0,0,0.75)", borderRadius: "16px" }} >
                     <h3 style={{ margin: "0" }}>Your payment status: </h3>
-                    <small>{paymentStatus}</small>
+                    {loading ?
+
+                        <span class="placeholder col-12"></span>
+                        :
+                        <small>{paymentStatus}</small>
+                    }
+
                     <br />
                     <br />
 
                     <h5 style={{ margin: "0" }}> Amount:  </h5>
-                    <small>INR {Number(paymentDetails?.amount) / 100}</small>
+                    {loading ?
+
+                        <span class="placeholder col-12"></span>
+                        :
+                        <small>INR {Number(paymentDetails?.amount) / 100}</small>
+                    }
+
                     <br />
                     <br />
-                    <h5 style={{ margin: "0" }}> UPI Transaction ID:  </h5>
-                    <small>{paymentDetails?.paymentInstrument?.upiTransactionId}</small>
+                    <h5 style={{ margin: "0" }}> IFSC:  </h5>
+                    {loading ?
+
+                        <span class="placeholder col-12"></span>
+                        :
+                        <small>{paymentDetails?.paymentInstrument?.ifsc}</small>
+                    }
+
                     <br />
                     <br />
                     <h5 style={{ margin: "0" }}> Account No:  </h5>
-                    <small>{paymentDetails?.paymentInstrument?.maskedAccountNumber}</small>
+                    {loading ?
+
+                        <span class="placeholder col-12"></span>
+                        :
+                        <small>{paymentDetails?.paymentInstrument?.maskedAccountNumber}</small>
+                    }
+
                     <br />
                     <br />
                     <h5 style={{ margin: "0" }}> Transaction ID:  </h5>
-                    <small>{paymentDetails?.transactionId}</small>
+                    {loading ?
+
+                        <span class="placeholder col-12"></span>
+                        :
+                        <small>{paymentDetails?.transactionId}</small>
+                    }
+
                     <br />
                     <br />
                     <button onClick={handleOkayClick} type="button" className="btn btn-dark download-button">
