dd9776b946e7035c4514dc9632bb4310f5a81834
Ajey <ajejey@gmail.com>
2023-06-09 12:50:21 +0530

    add skeleton

--- src/Components/Blog/IndexPage.js
+++ src/Components/Blog/IndexPage.js
@@ -3,6 +3,8 @@ import useSWR from 'swr'
 import Post from './Post'
 import Header from '../Header'
 import { useNavigate } from 'react-router-dom'
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 
 const fetcher = (url) => fetch(url).then((res) => res.json())
 
@@ -17,18 +19,27 @@ function IndexPage() {
         <div>
             <Header />
             <div className="container-md">
-                <h1 className='h1'>Blog</h1>
+                <h1 className="h1">Blog</h1>
                 <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
-                    <button onClick={handleClick} className='btn btn-primary btn-dark'>Create new post</button>
+                    <button
+                        onClick={handleClick}
+                        className="btn btn-primary btn-dark"
+                    >
+                        Create new post
+                    </button>
                 </div>
-                {isLoading && <div>Loading...</div>}
-                {error && <div>Error...</div>}
-                {posts?.data?.length && posts?.data.map((post) => (
-                    <Post key={post.id} {...post} />
-                ))}
+                {isLoading ? (
+                    // Skeleton loader
+                    <div>
+                        <Skeleton style={{ margin: '12px' }} count={4} height={150} />
+                    </div>
+                ) : error ? (
+                    <div>Error...</div>
+                ) : (
+                    posts?.data?.length &&
+                    posts?.data.map((post) => <Post key={post.id} {...post} />)
+                )}
             </div>
-
-
         </div>
     )
 }
