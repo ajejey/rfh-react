d9af1e58956e70b2aabb8c3fd2045f38203f9953
Ajey <ajejey@gmail.com>
2023-06-12 00:05:35 +0530

    admin create recipt

--- src/Components/Admin/AdminHome.js
+++ src/Components/Admin/AdminHome.js
@@ -1,32 +1,112 @@
 import { Tab, Tabs } from '@mui/material';
-import React, { useState } from 'react'
-import Header from '../Header'
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import useSWR from 'swr';
+import Header from '../Header';
 import EventParticipants from './EventParticipents';
 import VolunteerList from './VolunteerList';
 
 function AdminHome() {
-    const [tabNumber, setTabNumber] = useState(0)
+    const [tabNumber, setTabNumber] = useState(0);
+    const { handleSubmit, register, reset } = useForm();
+    const [downloadLink, setDownloadLink] = useState(null);
 
+    const body = [
+        'fullName',
+        'email',
+        'mobNo',
+        'PANno',
+        'donationAmount',
+    ];
+
+    const handleDonationFormSubmit = async (data) => {
+        const url = `${process.env.REACT_APP_BACKEND_BASE_URL}/create-receipt`;
+
+        try {
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
+            });
+
+            if (!response.ok) {
+                throw new Error('Failed to create receipt');
+            }
+
+            const responseData = await response.json();
+
+            // Extract the download link from the response
+            const { downloadLink } = responseData;
+
+            // Set the download link state to make it available for rendering
+            setDownloadLink(downloadLink);
+
+            // Reset the form
+            reset();
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
     const handleTabChange = (event, newValue) => {
-        setTabNumber(newValue)
-    }
+        setTabNumber(newValue);
+    };
+
+    const today = new Date().toISOString().split('T')[0]; // Get today's date as default value
 
     return (
         <div>
             <Header />
-            <div style={{ margin: "0 1%" }}>
-                <Tabs value={tabNumber} onChange={handleTabChange}>
+            <div className="container-md">
+                <h1 className="h1">Admin</h1>
+                <h3 className="h3">Update offline donations</h3>
+                <form onSubmit={handleSubmit(handleDonationFormSubmit)}>
+                    {body.map((field) => (
+                        <div className="mb-3" key={field}>
+                            <label htmlFor={field} className="form-label">{field}</label>
+                            <input
+                                type="text"
+                                id={field}
+                                className="form-control"
+                                {...register(field)} // Register the field with react-hook-form
+                            />
+                        </div>
+                    ))}
+                    <div className="mb-3">
+                        <label htmlFor="date" className="form-label">Date</label>
+                        <input
+                            type="date"
+                            id="date"
+                            defaultValue={today} // Set today's date as the default value
+                            className="form-control"
+                            {...register('date')} // Register the 'date' field with react-hook-form
+                        />
+                    </div>
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </form>
+                <br />
+                {downloadLink && (
+                    <div>
+                        <p>Download the invoice:</p>
+                        <div dangerouslySetInnerHTML={{ __html: downloadLink }} />
+                    </div>
+                )}
+            </div>
+        </div>
+    );
+}
+
+export default AdminHome;
+
+
+
+
+{/* <Tabs value={tabNumber} onChange={handleTabChange}>
                     <Tab label="Volunteers" value={0} />
                     <Tab label="Event Participant" value={1} />
                 </Tabs>
                 <br />
                 {tabNumber === 0 && <VolunteerList />}
-                {tabNumber === 1 && <EventParticipants />}
-
-            </div>
-        </div>
-    )
-}
-
-export default AdminHome
+                {tabNumber === 1 && <EventParticipants />} */}
--- src/Components/PrivateRoute.js
+++ src/Components/PrivateRoute.js
@@ -5,7 +5,36 @@ import useAuthStatus from '../CustomHooks/useAuthStatus'
 function PrivateRoute() {
     const { loggedIn, checkingStatus } = useAuthStatus();
     if (checkingStatus) {
-        return <h3>Loading...</h3>
+        const cardStyle = {
+            padding: '50px',
+            width: '300px',
+            margin: '0 auto',
+            marginTop: '20px',
+            borderRadius: '10px',
+            background: "#ffffff",
+            boxShadow: "15px 13px 34px 10px rgba(0,0,0,0.1)"
+        };
+
+        const titleStyle = {
+            fontSize: '20px',
+            fontWeight: 'bold',
+            marginBottom: '10px',
+        };
+
+        const textStyle = {
+            fontSize: '16px',
+        };
+        return (
+            <div style={{ height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                <div style={cardStyle}>
+                    <div>
+                        <h5 style={titleStyle}>Checking your credentials</h5>
+                        <p style={textStyle}>Please wait while we verify your credentials...</p>
+                    </div>
+                </div>
+
+            </div>
+        )
     }
     return loggedIn ? <Outlet /> : <Navigate to="/login" />
 }
